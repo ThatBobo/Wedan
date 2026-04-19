f296f1b79e584306535c496ae85d93209c892ac1
IamBobo <abdulsamad.arilewola@gmail.com>
2026-04-19 10:22:46 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <BrowserRouter basename="Wedan">
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
